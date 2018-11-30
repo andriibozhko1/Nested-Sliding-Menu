72964ca91f519798dad12d674a558c1f81f2d066
Andrii <andriibozhko1@gmail.com>
2018-11-30 13:14:43 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -1,95 +1,71 @@
 (function() {
-  let menu = [
+  const menu = [
     {
-      title: "HTML and CSS Week 1",
+      title: 'HTML and CSS Week 1',
       items: [
         {
-          title: "Day 1: First meeting",
-          link:
-            "https://github.com/mate-academy/fs-program/blob/master/lesson_00_instructions.md"
+          title: 'Day 1: First meeting',
+          link: 'https://github.com/mate-academy/fs-program/blob/master/lesson_00_instructions.md',
         },
         {
-          title: "Day 2: HTML",
-          week: 1,
-          link:
-            "https://github.com/mate-academy/fs-program/blob/master/html_css/lesson_11.md",
+          title: 'Day 2: HTML',
+          link: 'https://github.com/mate-academy/fs-program/blob/master/html_css/lesson_11.md',
           items: [
             {
-              title: "Algo main: Sum numbers in range",
-              week: 1,
-              link:
-                "https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-numbers-in-a-range"
+              title: 'Algo main: Sum numbers in range',
+              link: 'https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-numbers-in-a-range',
             },
             {
-              title: "Algo stretch: Seek and destroy",
-              week: 1,
-              link:
-                "https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/seek-and-destroy"
+              title: 'Algo stretch: Seek and destroy',
+              link: 'https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/seek-and-destroy',
             },
             {
-              title: "Project: Create html form",
-              week: 1,
-              link:
-                "https://github.com/mate-academy/fe-lesson-1-practice-form/blob/master/README.md"
+              title: 'Project: Create html form',
+              link: 'https://github.com/mate-academy/fe-lesson-1-practice-form/blob/master/README.md',
             }
-          ]
+          ],
         }
-      ]
+      ],
     },
     {
-      title: "HTML and CSS Week 2",
-      week: 2,
+      title: 'HTML and CSS Week 2',
       items: [
         {
-          title: "Day 1: npm, gulp, sass",
-          week: 2,
+          title: 'Day 1: npm, gulp, sass',
           items: [
             {
-              title: "Algo main: Sum numbers in range",
-              week: 2,
-              link:
-                "https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-numbers-in-a-range"
+              title: 'Algo main: Sum numbers in range',
+              link: 'https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-numbers-in-a-range',
             },
             {
-              title: "Algo stretch: Seek and destroy",
-              week: 2,
-              link:
-                "https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/seek-and-destroy"
+              title: 'Algo stretch: Seek and destroy',
+              link: 'https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/seek-and-destroy',
             },
             {
-              title: "Project: Create html form",
-              week: 2,
-              link:
-                "https://github.com/mate-academy/fe-lesson-1-practice-form/blob/master/README.md"
+              title: 'Project: Create html form',
+              link: 'https://github.com/mate-academy/fe-lesson-1-practice-form/blob/master/README.md',
             }
-          ]
+          ],
         },
         {
-          title: "Day 2: BEM",
-          week: 2,
-          link:
-            "https://github.com/mate-academy/fs-program/blob/master/html_css/lesson_11.md",
+          title: 'Day 2: BEM',
+          link: 'https://github.com/mate-academy/fs-program/blob/master/html_css/lesson_11.md',
           items: [
             {
-              title: "Algo main: Sum numbers in range",
-              week: 2,
-              link:
-                "https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-numbers-in-a-range"
+              title: 'Algo main: Sum numbers in range',
+              link: 'https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-numbers-in-a-range',
             },
             {
-              title: "Algo stretch: sum all primes",
-              week: 2
+              title: 'Algo stretch: sum all primes',
             },
             {
-              title: "Project: Create html form",
-              week: 2,
-              link:
-                "https://github.com/mate-academy/fe-lesson-1-practice-form/blob/master/README.md"
+              title: 'Project: Create html form',
+              link: 'https://github.com/mate-academy/fe-lesson-1-practice-form/blob/master/README.md',
             }
-          ]
+          ],
         }
-      ]
-    }
+      ],
+    },
   ];
 
   const createHeaders = function() {
